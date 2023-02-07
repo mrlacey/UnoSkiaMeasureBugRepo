3fd77f88a5e784d60ec9887adb4ccf2fdf9d24d1
Matt Lacey <matt@mrlacey.co.uk>
2023-02-07 15:19:57 +0000

    add repro

---
+++ UnoControlSizeBugRepo/UnoControlSizeBugRepo.Shared/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace UnoControlSizeBugRepo
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Btn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            Info.Children.Clear();
+            Info.Children.Add(new TextBlock() { Text = $"GridOfInterest: {GridOfInterest.ActualHeight} {GridOfInterest.ActualWidth}" });
+            Info.Children.Add(new TextBlock() { Text = $"InnerText: {InnerText.ActualHeight} {InnerText.ActualWidth}" });
+        }
+    }
+}
